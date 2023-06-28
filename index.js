aabdb5daba373322e2d5a6c1f8ae04fd45e44d26
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2023-06-28 15:32:00 +0530

    configured ports

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mysql = require('mysql');
 require('dotenv').config()
 
 const app = express();
-const port = process.env.MYSQL_PORT;
+const port = 3000;
+const sqlport = process.env.MYSQL_PORT;
 const user = process.env.MYSQL_USER;
 const password = process.env.MYSQL_PASSWORD;
 const database = process.env.MYSQL_DATABASE;
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port,
+    port: sqlport,
     user,
     password,
     database,
@@ -36,33 +37,30 @@ const connection = mysql.createConnection({
   
   // Endpoint to create a new item
 app.post('/items', (req, res) => {
-const newItem = req.body;
+  const newItem = req.body;
+  pool.getConnection((err, connection) => {
+      if (err) {
+          console.error('Error connecting to the database:', err);
+          res.status(500).json({ message: 'Internal Server Error' });
+          return;
+      }
+      // Perform the database query to save the item
+      connection.query('INSERT INTO items SET ?', newItem, (err, result) => {
+      connection.release();
 
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        res.status(500).json({ message: 'Internal Server Error' });
-        return;
-    }
-
-    // Perform the database query to save the item
-    connection.query('INSERT INTO items SET ?', newItem, (err, result) => {
-    connection.release();
-
-    if (err) {
-        console.error('Error saving item to the database:', err);
-        res.status(500).json({ message: 'Internal Server Error' });
-        return;
-    }
-
-    // Return a response indicating the item was created
-    res.status(201).json({ message: 'Item created successfully' });
-    });
-});
+      if (err) {
+          console.error('Error saving item to the database:', err);
+          res.status(500).json({ message: 'Internal Server Error' });
+          return;
+      }
+      // Return a response indicating the item was created
+      res.status(201).json({ message: 'Item created successfully' });
+      });
+  });
 });
   
   
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
+}); 
